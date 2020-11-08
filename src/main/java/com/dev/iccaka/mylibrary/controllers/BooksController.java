8cd0b4dee4d1d669fbce3ae468680dc549828035
Hristo Mitsev <dakillaroshun@gmail.com>
2020-11-08 04:07:48 +0200

    Add 'getBookById' inside 'BooksController'

--- src/main/java/com/dev/iccaka/mylibrary/controllers/BooksController.java
+++ src/main/java/com/dev/iccaka/mylibrary/controllers/BooksController.java
@@ -1,10 +1,30 @@
 package com.dev.iccaka.mylibrary.controllers;
 
+import com.dev.iccaka.mylibrary.repositories.BooksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/books")
 public class BooksController {
 
+    @Autowired
+    BooksRepository booksRepository;
+
+    @GetMapping("/search/id")
+    public String getBookById(@RequestParam(required = false) Long id){
+        String result = null;
+
+        try{
+            result = booksRepository.findById(id).toString();
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
+
+        return result;
+    }
 }
