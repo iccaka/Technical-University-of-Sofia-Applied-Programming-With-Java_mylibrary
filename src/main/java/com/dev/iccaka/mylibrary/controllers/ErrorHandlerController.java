8bc9a4cabebe45ece5e37649bc2773ccb1314f6c
Hristo Mitsev <dakillaroshun@gmail.com>
2020-11-09 19:41:26 +0200

    Update 'ErrorHandlerController'

--- src/main/java/com/dev/iccaka/mylibrary/controllers/ErrorHandlerController.java
+++ src/main/java/com/dev/iccaka/mylibrary/controllers/ErrorHandlerController.java
@@ -1,4 +1,22 @@
 package com.dev.iccaka.mylibrary.controllers;
 
-public class ErrorHandlerController {
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorHandlerController implements ErrorController {
+
+    public static final String PATH = "/error";
+
+    @RequestMapping(value = PATH)
+    public ResponseEntity<?> error(){
+        return ResponseEntity.badRequest().body("You've entered a wrong address.");
+    }
+
+    @Override
+    public String getErrorPath() {
+        return null;
+    }
 }
