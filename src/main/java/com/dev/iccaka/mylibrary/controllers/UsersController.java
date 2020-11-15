991b3bfaf6c5152009b32e1f0b1bc19da0a3a066
Hristo Mitsev <dakillaroshun@gmail.com>
2020-11-15 23:24:19 +0200

    Add pagination functionality
    The user can now get all of the other user's info and paginate it by
    providing the number of pages he/she wants and the number of result
    he/she wants per given page.

--- src/main/java/com/dev/iccaka/mylibrary/controllers/UsersController.java
+++ src/main/java/com/dev/iccaka/mylibrary/controllers/UsersController.java
@@ -3,10 +3,13 @@ package com.dev.iccaka.mylibrary.controllers;
 import com.dev.iccaka.mylibrary.entities.User;
 import com.dev.iccaka.mylibrary.repositories.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.data.domain.Pageable;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,6 +71,27 @@ public class UsersController {
         else {
             response.put("message", "Successfully edited.");
         }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/page")
+    public ResponseEntity<?> paginateUser(@RequestParam(defaultValue = "1") int currentPage,
+                                          @RequestParam(defaultValue = "5") int perPage){
+
+        // It's 'currentPage' - 1, because it starts counting from zero
+        Pageable pageable = (Pageable) PageRequest.of(currentPage - 1, perPage);
+        Page<User> users = usersRepository.findPageUsers(pageable);
+
+        if(users.isEmpty()){
+            return new ResponseEntity<>("There aren't any results.", HttpStatus.NO_CONTENT);
+        }
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("users", users.getContent());
+        response.put("currentPage", users.getNumber());
+        response.put("totalNumbers", users.getTotalElements());
+        response.put("totalPages", users.getTotalPages());
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/dev/iccaka/mylibrary/repositories/UsersRepository.java
+++ src/main/java/com/dev/iccaka/mylibrary/repositories/UsersRepository.java
@@ -1,13 +1,18 @@
 package com.dev.iccaka.mylibrary.repositories;
 
 import com.dev.iccaka.mylibrary.entities.User;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import org.springframework.data.domain.Pageable;
 import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE lower(u.firstname) = :firstname")
     Optional<User> findUserByFirstname(String firstname);
+
+    @Query("SELECT u FROM User u")
+    Page<User> findPageUsers(Pageable pageable);
 }
